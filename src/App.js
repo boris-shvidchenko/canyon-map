a3276f21a28d6bdf3d27cf797dfb658921186850
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-01-14 17:20:53 -0800

    Made updates to App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import esriConfig from '@arcgis/core/config';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
-import { useLayoutEffect } from 'react';
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import { useEffect } from 'react';
 
 export default function App() {
 
-  useLayoutEffect(() => {
-    esriConfig.apiKey = process.env.REACT_APP_DEFAULT_API_KEY;
+  useEffect(() => {
+    esriConfig.apiKey = process.env.REACT_APP_DEFAULT_KEY;
   
     const map = new Map({
       basemap: 'arcgis-topographic' // Basemap layer service
@@ -23,6 +24,13 @@ export default function App() {
           spatialReference: 4326
       },
     });
+
+    const feature = new FeatureLayer({
+      url: 'https://services5.arcgis.com/K2yvD247JkyVgWmg/arcgis/rest/services/canyons/FeatureServer/0'
+    })
+
+    map.add(feature, 0)
+
   }, [])
 
   return (
