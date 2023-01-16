20913105d5b249225abfaba3e55791ddcfdebad5
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-01-15 17:27:17 -0800

    Began testing 3D view, temporary changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,7 @@
 import esriConfig from '@arcgis/core/config';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
+import SceneView from '@arcgis/core/views/SceneView';
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 // Hooks
 import { useEffect } from 'react';
@@ -14,25 +15,38 @@ export default function MainMap() {
   
     // Create a new map
     const map = new Map({
-      basemap: 'arcgis-topographic' // Basemap layer service
+      basemap: 'arcgis-topographic', 
+      ground: 'world-elevation'
     });
   
     // Create a new map view to hold/display the map
-    const view = new MapView({
-      container: 'root',
-      map: map,
-      extent: {
-          xmin: -108.149414,
-          ymin: 36.450743,
-          xmax: -115.092773,
-          ymax: 42.19054,
-          spatialReference: 4326
-      },
-      // Removes the + and - widget from the map
-      ui: {
-        components: ["attribution"]
-      }
-    });
+    // const view = new MapView({
+    //   container: 'root',
+    //   map: map,
+    //   extent: {
+    //       xmin: -108.149414,
+    //       ymin: 36.450743,
+    //       xmax: -115.092773,
+    //       ymax: 42.19054,
+    //       spatialReference: 4326
+    //   },
+    //   // Removes the + and - widget from the map
+    //   ui: {
+    //     components: ["attribution"]
+    //   }
+    // });
+
+    const view3D = new SceneView({
+        container: 'root',
+        map: map,
+        camera: {
+            position: {
+                x: -111.513936,
+                y: 39.133887,
+                z: 2000000
+            }
+        }
+    })
 
     // Create a template for popups
     const popupTemplate = {
@@ -67,14 +81,14 @@ export default function MainMap() {
     map.add(feature, 0);
 
     // Customize the popup
-    view.popup = {
-        dockOptions: {
-            buttonEnabled: false
-        },
-        viewModel: {
-            includeDefaultActions: false,
-        }
-    };
+    // view.popup = {
+    //     dockOptions: {
+    //         buttonEnabled: false
+    //     },
+    //     viewModel: {
+    //         includeDefaultActions: false,
+    //     }
+    // };
 
   }, [])
 
