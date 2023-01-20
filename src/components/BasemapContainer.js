9d9c13b1eac079b79df4a87f7612515d87e83ef9
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-01-19 21:52:44 -0800

    Moved long TW styles to seperate variables for the basemap container component

--- src/components/BasemapContainer.js
+++ src/components/BasemapContainer.js
@@ -13,19 +13,24 @@ export default function BasemapContainer() {
         dispatch({ type: 'updateBasemap', basemap: newBasemap})
     }
 
+    // Styles
+    const topoImgStyle = state.basemap === 'arcgis-topographic' ? 'border-2 border-red-500 hover:none' : 'border-2 border-transparent hover:border-[#ff6a00]';
+    const imageryImgStyle = state.basemap === 'arcgis-imagery' ? 'border-2 border-red-500 hover:border-red-500' : 'border-2 border-transparent hover:border-[#ff6a00]';
+    const streetsImgStyle = state.basemap === 'arcgis-streets-relief' ? 'border-2 border-red-500 hover:border-red-500' : 'border-2 border-transparent hover:border-[#ff6a00]';
+
     return (
-        <div className='w-60 h-20 bg-[#40404080] absolute bottom-7 left-4 flex space-x-2 p-2 rounded-sm'>
-            <section onClick={() => updateBasemap('arcgis-topographic')} className='w-1/3 flex flex-col items-center overflow-auto cursor-pointer'>
-                <img className={`w-full h-full ${state.basemap === 'arcgis-topographic' ? 'border-2 border-red-500 hover:none' : 'border-2 border-transparent hover:border-[#ff6a00]'}`} src='/images/basemap-thumbnails/topo.png' alt='Topographic' />
-                <p className='absolute -bottom-1 px-1 bg-[#404040] w-[3.85rem] text-center rounded-sm text-white font-semibold text-sm'>Topo</p>
+        <div className='basemap-container-main-div'>
+            <section onClick={() => updateBasemap('arcgis-topographic')} className='basemap-container-section'>
+                <img className={`w-full h-full ${topoImgStyle}`} src='/images/basemap-thumbnails/topo.png' alt='Topographic' />
+                <p className='basemap-container-map-name'>Topo</p>
             </section>
-            <section onClick={() => updateBasemap('arcgis-imagery')} className='w-1/3 flex flex-col items-center overflow-hidden cursor-pointer'>
-                <img className={`w-full h-full ${state.basemap === 'arcgis-imagery' ? 'border-2 border-red-500 hover:border-red-500' : 'border-2 border-transparent hover:border-[#ff6a00]'}`} src='/images/basemap-thumbnails/imagery.png' alt='Imagery' />
-                <p className='absolute -bottom-1 px-1 bg-[#404040] w-[3.85rem] text-center rounded-sm text-white font-semibold text-sm'>Imagery</p>
+            <section onClick={() => updateBasemap('arcgis-imagery')} className='basemap-container-section'>
+                <img className={`w-full h-full ${imageryImgStyle}`} src='/images/basemap-thumbnails/imagery.png' alt='Imagery' />
+                <p className='basemap-container-map-name'>Imagery</p>
             </section>
-            <section onClick={() => updateBasemap('arcgis-streets-relief')} className='w-1/3 flex flex-col items-center overflow-hidden cursor-pointer'>
-                <img className={`w-full h-full ${state.basemap === 'arcgis-streets-relief' ? 'border-2 border-red-500 hover:border-red-500' : 'border-2 border-transparent hover:border-[#ff6a00]'}`} src='/images/basemap-thumbnails/streets.png' alt='Streets' />
-                <p className='absolute -bottom-1 px-1 bg-[#404040] w-[3.85rem] text-center rounded-sm text-white font-semibold text-sm'>Streets</p>
+            <section onClick={() => updateBasemap('arcgis-streets-relief')} className='basemap-container-section'>
+                <img className={`w-full h-full ${streetsImgStyle}`} src='/images/basemap-thumbnails/streets.png' alt='Streets' />
+                <p className='basemap-container-map-name'>Streets</p>
             </section>
         </div>
     )
