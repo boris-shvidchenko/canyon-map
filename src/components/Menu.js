a9f840217898dabdf67b3dd78088f8eab61a945e
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-01-25 14:04:36 -0800

    Cleaned up menu component in JSX and added a sidebar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import { useContext } from 'react'
 // Context
 import { Context } from '../App'
 // Heroicons
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 export default function Menu() {
 
@@ -17,11 +17,15 @@ export default function Menu() {
 
     return (
         <div>
-            {state.menuIconVisible && 
-                <div className='absolute top-6 right-6 bg-[#404040] p-1 rounded-sm'>
-                    <Bars3Icon onClick={toggleMenu} className='w-9 h-9 cursor-pointer text-white hover:text-[#ff6a00]' />
-                </div>
-            }
+
+            <div className={`${!state.menuIconVisible && 'hidden'} absolute top-6 right-6 bg-[#404040] p-1 rounded-sm`}>
+                <Bars3Icon onClick={toggleMenu} className='w-9 h-9 cursor-pointer text-white hover:text-[#ff6a00]' />
+            </div>
+
+            <div className={`${state.menuIconVisible && 'fixed top-0 -right-96'} ${!state.menuIconVisible && 'fixed top-0 right-0'} w-96 h-[30rem] bg-[#404040] z-10`}>
+                <XMarkIcon onClick={toggleMenu} className='absolute top-7 right-7 w-9 h-9 cursor-pointer text-white hover:text-[#ff6a00]' />
+            </div>
+            
         </div>
     )
 }
