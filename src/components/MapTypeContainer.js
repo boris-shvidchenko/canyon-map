afd1c3c4ebcdec33976db21c518adbb61f0d7bb0
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-01-30 21:48:12 -0800

    Made map type container responsive

--- src/components/MapTypeContainer.js
+++ src/components/MapTypeContainer.js
@@ -16,7 +16,7 @@ export default function MapTypeContainer() {
     }
 
     return (
-        <div className='w-10 h-20 bg-[#40404080] absolute bottom-8 left-[16.75rem] p-1 rounded-sm flex flex-col justify-around'>
+        <div className={`bg-[#40404080] absolute flex justify-around items-center ${state.screenWidth < 320 ? 'w-20 h-11 bottom-[7.75rem] left-4' : 'w-11 h-20 bottom-8 left-[16.75rem] flex-col'} p-1 rounded-sm`}>
             <section onClick={(e) => changeMapType(e)} className={`bg-[#404040] text-white rounded-sm w-8 h-8 flex justify-center items-center cursor-pointer hover:text-[#ff6a00] ${state.twoDimensional && 'text-[#ff6a00]'}`}>
                 <p className='text-lg font-semibold'>2D</p>
             </section>
