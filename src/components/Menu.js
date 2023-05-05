65503ae0cf509869b49102dc995df00c795cfc88
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-05-05 13:25:29 -0700

    Date updated

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -59,7 +59,7 @@ export default function Menu() {
                 {/* Site info */}
                 <section className='menu-info'>
                     <p>Developed by: Boris Shvidchenko</p>
-                    <p>Last update: 05/04/2023</p>
+                    <p>Last update: 05/05/2023</p>
                 </section>
             </div>
         </div>
