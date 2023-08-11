477b941272583e874bee635e4faa7d383c2b6b4c
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-08-10 20:10:12 -0700

    Cleaned up filter container

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -13,6 +13,7 @@ export default function Filter() {
     const { state, dispatch } = useContext(Context);
 
     // Styles
+    const filterIcon = !state.filterIconVisible && 'hidden';
     const filterVisible = !state.filterIconVisible && 'fixed top-0 right-0';
     const filterNotVisible = state.filterIconVisible && 'fixed top-0 -right-96';
     const width = state.screenWidth < 390 && 'w-screen h-screen overflow-y-scroll overflow-x-hidden scrollbar-thin scrollbar-thumb-[#8a8a8a] pb-6';
@@ -20,12 +21,14 @@ export default function Filter() {
 
     return (
         <div>
-            <div onClick={() => toggleFilter(dispatch)} className={`filter-icon-container`}>
+            <div onClick={() => toggleFilter(dispatch)} className={`${filterIcon} filter-icon-container`}>
                 <FunnelIcon className='filter-icon' />
             </div>
-            <div className={`menu-container ${filterNotVisible} ${filterVisible} ${width} ${height}`}>
-                <XMarkIcon onClick={() => toggleFilter(dispatch)} className='menu-x' />
-                <p className='text-white'>Filter options here...</p>
+            <div className={`filter-container ${filterNotVisible} ${filterVisible} ${width} ${height}`}>
+                <section className='filter-container-section'>
+                    <p className='text-[#ff6a00]'>Add Filter</p>
+                    <XMarkIcon onClick={() => toggleFilter(dispatch)} className='menu-x' />
+                </section>
             </div>
         </div>
     )
