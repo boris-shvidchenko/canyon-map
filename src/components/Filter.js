a96fb8952017253268b8f3f30fab5ab4851256e1
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2023-09-16 09:13:21 -0700

    Cleaned up filter component styles, movedto custom variables

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -18,6 +18,8 @@ export default function Filter() {
     const filterNotVisible = state.filterIconVisible && 'fixed top-0 -right-96';
     const width = state.screenWidth < 390 && 'w-screen h-screen overflow-y-scroll overflow-x-hidden scrollbar-thin scrollbar-thumb-[#8a8a8a] pb-6';
     const height = state.screenHeight < 660 && 'h-screen overflow-y-scroll scrollbar-thin scrollbar-thumb-[#8a8a8a] pb-6';
+    const gridWidth = state.screenWidth > 300 && 'w-64';
+    const gridSection = state.screenWidth < 300 ? 'space-x-1 flex' : 'space-x-2 flex';
 
     // Input references
     const [techFilter2, techFilter3, techFilter4] = [useRef(), useRef(), useRef()];
@@ -39,57 +41,57 @@ export default function Filter() {
                 </section>
                 <section className='ml-8 pt-4'>
                     <p className='menu-text'>Search canyons by their ratings:</p>
-                    <div className={`grid grid-cols-5 gap-1 ${state.screenWidth > 300 && 'w-64'} pt-2`}>
-                        <h3 className='text-[#fff] text-sm'>Technical</h3>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex col-start-3`}>
-                            <input type='checkbox' id='two' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, techFilterArray)} ref={techFilter2} />
-                            <label htmlFor='' className='text-[#cfcfcf] text-sm'>2</label>
+                    <div className={`${gridWidth} filter-grid-div`}>
+                        <h3 className='filter-grid-h3'>Technical</h3>
+                        <section className={`${gridSection} col-start-3`}>
+                            <input type='checkbox' id='two' className='filter-grid-input' onClick={(e) => updateFilter(e, techFilterArray)} ref={techFilter2} />
+                            <label htmlFor='' className='filter-grid-label'>2</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex`}>
-                            <input type='checkbox' id='three' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, techFilterArray)} ref={techFilter3} />
-                            <label className='text-[#cfcfcf] text-sm'>3</label>
+                        <section className={`${gridSection}`}>
+                            <input type='checkbox' id='three' className='filter-grid-input' onClick={(e) => updateFilter(e, techFilterArray)} ref={techFilter3} />
+                            <label className='filter-grid-label'>3</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex`}>
-                            <input type='checkbox' id='four' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, techFilterArray)} ref={techFilter4} />
-                            <label className='text-[#cfcfcf] text-sm'>4</label>
+                        <section className={`${gridSection}`}>
+                            <input type='checkbox' id='four' className='filter-grid-input' onClick={(e) => updateFilter(e, techFilterArray)} ref={techFilter4} />
+                            <label className='filter-grid-label'>4</label>
                         </section>
-                        <h3 className='text-[#fff] text-sm'>Water</h3>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex col-start-3`}>
-                            <input type='checkbox' id='A' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, waterFilterArray)} ref={waterFilterA}  />
-                            <label className='text-[#cfcfcf] text-sm'>A</label>
+                        <h3 className='filter-grid-h3'>Water</h3>
+                        <section className={`${gridSection} col-start-3`}>
+                            <input type='checkbox' id='A' className='filter-grid-input' onClick={(e) => updateFilter(e, waterFilterArray)} ref={waterFilterA}  />
+                            <label className='filter-grid-label'>A</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex`}>
-                            <input type='checkbox' id='B' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, waterFilterArray)} ref={waterFilterB}  />
-                            <label className='text-[#cfcfcf] text-sm'>B</label>
+                        <section className={`${gridSection}`}>
+                            <input type='checkbox' id='B' className='filter-grid-input' onClick={(e) => updateFilter(e, waterFilterArray)} ref={waterFilterB}  />
+                            <label className='filter-grid-label'>B</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex`}>
-                            <input type='checkbox' id='C' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, waterFilterArray)} ref={waterFilterC}  />
-                            <label className='text-[#cfcfcf] text-sm'>C</label>
+                        <section className={`${gridSection}`}>
+                            <input type='checkbox' id='C' className='filter-grid-input' onClick={(e) => updateFilter(e, waterFilterArray)} ref={waterFilterC}  />
+                            <label className='filter-grid-label'>C</label>
                         </section>
-                        <h3 className='text-[#fff] text-sm'>Time</h3>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex col-start-3`}>
-                            <input type='checkbox' id='I' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterI}  />
-                            <label className='text-[#cfcfcf] text-sm'>I</label>
+                        <h3 className='filter-grid-h3'>Time</h3>
+                        <section className={`${gridSection} col-start-3`}>
+                            <input type='checkbox' id='I' className='filter-grid-input' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterI}  />
+                            <label className='filter-grid-label'>I</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex`}>
-                            <input type='checkbox' id='II' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterII}  />
-                            <label className='text-[#cfcfcf] text-sm'>II</label>
+                        <section className={`${gridSection}`}>
+                            <input type='checkbox' id='II' className='filter-grid-input' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterII}  />
+                            <label className='filter-grid-label'>II</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex`}>
-                            <input type='checkbox' id='III' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterIII}  />
-                            <label className='text-[#cfcfcf] text-sm'>III</label>
+                        <section className={`${gridSection}`}>
+                            <input type='checkbox' id='III' className='filter-grid-input' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterIII}  />
+                            <label className='filter-grid-label'>III</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex col-start-3`}>
-                            <input type='checkbox' id='IV' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterIV}  />
-                            <label className='text-[#cfcfcf] text-sm'>IV</label>
+                        <section className={`${gridSection} col-start-3`}>
+                            <input type='checkbox' id='IV' className='filter-grid-input' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterIV}  />
+                            <label className='filter-grid-label'>IV</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex`}>
-                            <input type='checkbox' id='V' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterV}  />
-                            <label className='text-[#cfcfcf] text-sm'>V</label>
+                        <section className={`${gridSection}`}>
+                            <input type='checkbox' id='V' className='filter-grid-input' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterV}  />
+                            <label className='filter-grid-label'>V</label>
                         </section>
-                        <section className={`${state.screenWidth < 300 ? 'space-x-1' : 'space-x-2'} flex`}>
-                            <input type='checkbox' id='VI' className='cursor-pointer accent-[#ff6a00]' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterVI}  />
-                            <label className='text-[#cfcfcf] text-sm'>VI</label>
+                        <section className={`${gridSection}`}>
+                            <input type='checkbox' id='VI' className='filter-grid-input' onClick={(e) => updateFilter(e, timeFilterArray)} ref={timeFilterVI}  />
+                            <label className='filter-grid-label'>VI</label>
                         </section>
                     </div>
                 </section>
@@ -105,6 +107,6 @@ export default function Filter() {
 }
 
 // To Do:
-// 6. Finalize styles and move them to index.css as custom tailwind variables
-// 7. Connect to state to apply filters to map
-// 8. Connect the 'clear' button to remove all checks
+// 1. Connect to state to apply filters to map, clear checks on select of 'Apply'
+// 2. Connect the 'clear' button to remove all checks
+// 3. Optional: if options are selected and user closes window with 'X', deselect?
